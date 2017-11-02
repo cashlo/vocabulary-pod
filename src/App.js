ebdeba325408b17bbee10cb0b968cbe5967b3917
Cash T.H. Lo <tinghin.lo@booking.com>
2017-11-02 15:14:43 +0100

    Add import from Duo

--- src/App.js
+++ src/App.js
@@ -6,11 +6,40 @@ import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import VocabularyList from './VocabularyList';
 import AppBar from 'material-ui/AppBar';
+import VocabularyActionButton from './VocabularyActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
 
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      vocabularies: []
+    }
+  }
+
+  addVocabulary = word => {
+    this.setState(function(pState, props){
+      var nv = pState.vocabularies;
+      nv.push({text: word});
+      return { vocabularies: nv };
+    });
+  }
+
+  updateVocabulary = words => {
+    this.setState(function(pState, props){
+      var nv = pState.vocabularies;
+      words.forEach(word => {
+        nv.push({text: word});
+      });
+      nv.sort();
+      return { vocabularies: nv };
+    });
+  }
+
   render() {
     return (
       <MuiThemeProvider>
@@ -18,7 +47,8 @@ class App extends Component {
         <AppBar
           title="Vocabulary Pod"
         />
-        <VocabularyList/>
+        <VocabularyList vocabularies={this.state.vocabularies} onEnterNewWord={this.addVocabulary}/>
+        <VocabularyActionButton onVocabularyUpdate={this.updateVocabulary}/>
       </div>
       </MuiThemeProvider>
     );
--- src/VocabularyList.js
+++ src/VocabularyList.js
@@ -3,28 +3,17 @@ import {List, ListItem} from 'material-ui/List';
 import TextField from 'material-ui/TextField';
 
 class VocabularyList extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			vocabularies: []
-		}
-	}
 
 	handleKeyPress = (e => {
 		if (e.key === 'Enter') {
-			var newWord = e.target.value;
-			this.setState(function(pState, props){
-				var nv = pState.vocabularies;
-				nv.push({text: newWord});
-				return { vocabularies: nv };
-			});
+			this.props.onEnterNewWord(e.target.value);
 			e.target.value = "";
     	}
     })
 
 	render() {
 		const rows = [];
-		this.state.vocabularies.forEach(function(word) {
+		this.props.vocabularies.forEach(function(word) {
 			rows.push(
 				<ListItem primaryText={word.text}/>
 			);
